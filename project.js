72ba7ddf56059f0e6818c94dd2ffec6992f7633e
Hassan-abdi-dev <78468520+Hassan-abdi-dev@users.noreply.github.com>
2021-02-19 18:02:06 +0300

    Add files via upload

---
+++ project.js
@@ -0,0 +1,15 @@
+//birthyear
+function ageInDays(){
+    var birthYear=prompt("What year were you born");
+    var ageInDayss=(2021-birthYear)*1;
+    var h1=document.createElement ("h1");
+    var textAnswer=document.createTextNode("You are"+ageInDayss+"Years old");
+    h1.setAttribute("id","ageInDays");
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    
+}
+function reset(){
+    document.getElementById("ageInDays").remove();
+    
+}
